f03b28f02a07376ed6b2f541c4eaad6720360724
RakeshJakka5158 <rakeshjakka5158@gmail.com>
2024-06-01 07:11:11 +0530

    Initial commit

---
+++ src/test/java/com/acn/student/studentuplaod/StudentUplaodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn.student.studentuplaod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentUplaodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
